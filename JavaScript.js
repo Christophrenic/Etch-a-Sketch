38758fec3ea6db4f28e9e712a089a4ef6227e812
Christophrenic <panth300@gmail.com>
2022-03-06 15:43:22 +0100

    Create CSS & flexbox formatted default 16x16 grid
    Creates a gridContainer div, containing within it 256 grid elements.
    Applies flexbox & CSS sizing for proper formatting.

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+function createDefaultGrid() {                                 //createDefaultGrid() Creates 256 grids within gridContainer (16x16)
+    const gridContainer = document.createElement('div');
+        gridContainer.classList.add('gridContainer');
+        document.body.appendChild(gridContainer)
+
+    let i = 0;                    
+    while (i < 256) {                                          //Create grids and append inside gridContainer
+        const defaultGrid = document.createElement('div');  
+        defaultGrid.classList.add('gridDiv');
+        gridContainer.appendChild(defaultGrid);
+        i++;
+    }
+    return;
+}
+
+createDefaultGrid();
